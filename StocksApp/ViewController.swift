a87e6fe4780624941f1d361f20d1f374e146a382
Gl1mmer <gl1mmer292409@gmail.com>
2024-11-09 19:52:12 +0500

    commit 0

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Amankeldi Zhetkergen on 09.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
